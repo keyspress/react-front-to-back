e3aa3a2b2b6d08d68e213ae6a841fd591aaf5631
Kyle Desktop <kylespresser@gmail.com>
2018-09-11 12:22:10 -0400

    lecture 20

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Header from './components/Header';
 import Contact from './components/Contact';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Component</h1>
-        <Contact />
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="jdoe@gmail.com"
+            phone="555-555-5555"
+          />
+          <Contact
+            name="Karen Smith"
+            email="karen@gmail.com"
+            phone="444-444-5555"
+          />
+        </div>
       </div>
     );
   }

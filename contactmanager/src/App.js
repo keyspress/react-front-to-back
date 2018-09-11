01724b3c5f61df2280c6c3ceb38df02458b5ecc8
Kyle Desktop <kylespresser@gmail.com>
2018-09-11 11:49:27 -0400

    lecture 15

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Contact from './components/Contact';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>The App Component</h1>
+        <Contact />
       </div>
     );
   }

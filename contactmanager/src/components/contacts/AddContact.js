4c4df503c4f1a179bd3a745e551f1a755bc26560
Kyle Desktop <kylespresser@gmail.com>
2018-09-13 11:45:19 -0400

    lecture 38

--- contactmanager/src/components/contacts/AddContact.js
+++ contactmanager/src/components/contacts/AddContact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import uuid from 'uuid';
+import axios from 'axios';
 
 import { Consumer } from '../../context';
 import TextInputGroup from '../layout/TextInputGroup';
@@ -18,7 +18,6 @@ class AddContact extends Component {
     const { name, email, phone } = this.state;
 
     const newContact = {
-      id: uuid(),
       name,
       email,
       phone
@@ -39,7 +38,9 @@ class AddContact extends Component {
       return;
     }
 
-    dispatch({ type: 'ADD_CONTACT', payload: newContact });
+    axios
+      .post('https://jsonplaceholder.typicode.com/users', newContact)
+      .then(res => dispatch({ type: 'ADD_CONTACT', payload: res.data }));
 
     this.setState({ name: '', email: '', phone: '', errors: {} });
 

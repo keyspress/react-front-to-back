41a49684a084a431f9cfa6d672d0ce549e146e06
Kyle Desktop <kylespresser@gmail.com>
2018-09-13 12:05:06 -0400

    lecture 39

--- contactmanager/src/components/contacts/AddContact.js
+++ contactmanager/src/components/contacts/AddContact.js
@@ -12,7 +12,7 @@ class AddContact extends Component {
     errors: {}
   };
 
-  onSubmit = (dispatch, e) => {
+  onSubmit = async (dispatch, e) => {
     e.preventDefault();
 
     const { name, email, phone } = this.state;
@@ -38,9 +38,12 @@ class AddContact extends Component {
       return;
     }
 
-    axios
-      .post('https://jsonplaceholder.typicode.com/users', newContact)
-      .then(res => dispatch({ type: 'ADD_CONTACT', payload: res.data }));
+    const res = await axios.post(
+      'https://jsonplaceholder.typicode.com/users',
+      newContact
+    );
+
+    dispatch({ type: 'ADD_CONTACT', payload: res.data });
 
     this.setState({ name: '', email: '', phone: '', errors: {} });
 

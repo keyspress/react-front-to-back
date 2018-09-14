04316bf86d301e27396cf9f7b009a38c1cb4361e
Kyle Desktop <kylespresser@gmail.com>
2018-09-14 09:30:31 -0400

    lecture 45

--- contactmanager_redux/src/App.js
+++ contactmanager_redux/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact';
 import EditContact from './components/contacts/EditContact';
@@ -7,7 +9,6 @@ import Header from './components/layout/Header';
 import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
 
-import { Provider } from 'react-redux';
 import store from './store';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
